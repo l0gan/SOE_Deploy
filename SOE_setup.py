3a9c1af3c58e86741204f8617ec17f13f085a59c
Kirk Hayes <l0gan@users.noreply.github.com>
2019-05-21 08:42:08 -0400

    Windows/Linux/Mac SOE Deployment
    Will work with Mac, Windows, or Linux hosts

--- SOE_setup.py
+++ SOE_setup.py
@@ -4,11 +4,15 @@ import subprocess
 import time
 from datetime import datetime
 import configparser
+import os
+import platform
 
 # This script will work in conjunction with Ansible to configure a Red Team/PenTesting SOE
 # The python script will clone the base VM, start up the VM, obtain the IP, and kick off the Ansible Playbook
 # Variables
 startTime = datetime.now().strftime('%Y-%m-%d %H:%M:%S') # helps give us information on how much time it took to run
+ovftool_path = [r"c:\Program Files\VMware\VMware OVF Tool"]
+vmrun_path = [r"c:\Program Files (x86)\VMware\VMware Workstation"]
 
 def config():
     config = configparser.ConfigParser()
@@ -20,12 +24,17 @@ def config():
     return basePath, OVATemplate, privKeyLoc, SOEuser
 
 # Clone VM
-def cloneBaseVM(customerName, basePath, OVATemplate, privKeyLoc, SOEuser):
+def cloneBaseVM(customerName, basePath, OVATemplate, privKeyLoc, SOEuser, osArch):
     print("[+] Deploying Base VM from OVA")
     # Need OVFTool from here: https://my.vmware.com/web/vmware/details?productId=352&downloadGroup=OVFTOOL350
     # /Applications/VMware\ OVF\ Tool/ovftool --allowExtraConfig --lax <path_to_ovf_file> <path_to_target_folder>
-    args = "--allowExtraConfig --lax --name=" + customerName + "-Ubuntu " + basePath + OVATemplate + " " + basePath + customerName + "/VM/"
-    subprocess.call("/Applications/VMware\ OVF\ Tool/ovftool " + args, shell=True)
+    args = "--allowExtraConfig --lax --name==" + customerName + "-Ubuntu " + basePath + OVATemplate + " " + basePath + customerName + "/VM/"
+    if osArch == 'win':
+        subprocess.call([ovftool_path[0] + "\ovftool.exe", "--allowExtraConfig", "--lax", "--name=" + customerName + "-Ubuntu", basePath + OVATemplate, basePath + customerName + "\VM"])
+    elif osArch == 'linux':
+        subprocess.call("ovftool " + args, shell=True)
+    elif osArch == 'mac':
+        subprocess.call("/Applications/VMware\ OVF\ Tool/ovftool " + args, shell=True)
     print("[+] VM Cloned")
 
 def cusFolderSetup(basePath):
@@ -45,40 +54,72 @@ def cusFolderSetup(basePath):
     return customerName
 
 # Start VM
-def startVM(customerName, basePath, OVATemplate, privKeyLoc, SOEuser):
+def startVM(customerName, basePath, OVATemplate, privKeyLoc, SOEuser, osArch):
     print("[+] Starting VM")
     # vmrun -T fusion start <path_to_file.vmx>
-    subprocess.call("vmrun -T fusion start " + basePath + customerName + "/VM/" + customerName + "-Ubuntu" + ".vmwarevm/" + customerName + "-Ubuntu" + ".vmx", shell=True)
+    if osArch == 'win':
+        arg1 = vmrun_path[0] + "\\vmrun.exe"
+        arg2 = "-T"
+        arg3 = "ws" #change to fusion if Mac
+        arg4 = "start"
+        arg5 = basePath + customerName + "\\VM\\" + customerName + "-Ubuntu\\" + customerName + "-Ubuntu" + ".vmx"
+        subprocess.call([arg1, arg2, arg3, arg4, arg5], shell=True)
+    elif osArch == 'linux':
+        subprocess.call("vmrun -T ws start " + basePath + customerName + "/VM/" + customerName + "-Ubuntu" + ".vmwarevm/" + customerName + "-Ubuntu" + ".vmx", shell=True)
+    elif osArch == 'mac':
+        subprocess.call("vmrun -T fusion start " + basePath + customerName + "/VM/" + customerName + "-Ubuntu" + ".vmwarevm/" + customerName + "-Ubuntu" + ".vmx", shell=True)
     print("[!] Waiting a minute to let the VM fully start...")
     time.sleep(60)
     print("[+] VM Should be Started")
 
 # Determine VM IP
-def vmIPlookup(customerName, basePath, OVATemplate, privKeyLoc, SOEuser):
+def vmIPlookup(customerName, basePath, OVATemplate, privKeyLoc, SOEuser, osArch):
     print("[+] Determining VM IP and updating hostlist")
     # vmrun getGuestIPAddress <path_to_file.vmx>
-    vm_ip = subprocess.check_output("vmrun getGuestIPAddress " + basePath + customerName + "/VM/" + customerName + "-Ubuntu" + ".vmwarevm/" + customerName + "-Ubuntu" + ".vmx", shell=True)
+    if osArch == 'win':
+        vm_ip = subprocess.check_output([vmrun_path[0] + "\\vmrun.exe", "getGuestIPAddress", basePath + customerName + "\\VM\\" + customerName + "-Ubuntu\\" + customerName + "-Ubuntu" + ".vmx"], shell=True)
+    else:
+        vm_ip = subprocess.check_output("vmrun getGuestIPAddress " + basePath + customerName + "/VM/" + customerName + "-Ubuntu" + ".vmwarevm/" + customerName + "-Ubuntu" + ".vmx", shell=True)
     vm_ip = str(vm_ip).split("'")[1]
     vm_ip = vm_ip.split("\\n")[0]
+    if osArch == 'win':
+        vm_ip = vm_ip.split("\\r")[0]
     return vm_ip
 
-def ansibleFun(vm_ip, basePath, OVATemplate, privKeyLoc, SOEuser):
+def discoverOsArch():
+    # Figure out OS Architecture because Windows pathing is so much different...
+    print("[+] Discovering OS Architecture")
+    osType = platform.system()
+    if osType == "Windows":
+        osArch = 'win'
+    elif osType == "Linux":
+        osArch = 'linux'
+    else:
+        osArch = 'mac'
+    print("[+] Running on " + osType)
+    return osArch
+
+def ansibleFun(vm_ip, basePath, OVATemplate, privKeyLoc, SOEuser, osArch):
     # Write IP to hosts file
     f = open("hosts.txt", "w")
     f.write(vm_ip)
     f.close()
     print("[+] Your VM appears to be at " + vm_ip)
     # Start Ansible Playbook
-    print("[+] Running Ansible Playlist...This may take some time...Go grab a coffee!")
-    subprocess.call("ansible-playbook -i hosts.txt -become -u " + SOEuser + " --private-key=" + privKeyLoc + " -K main.yml", shell=True)
+    print("[+] Running Ansible Playlist...This may take some time...Just need two bits of information, then go grab a coffee!")
+    if osArch == 'win':
+        subprocess.call('bash -c "ansible-playbook -i hosts.txt -become -u ' + SOEuser + ' --private-key=' + privKeyLoc + ' -K main.yml"', shell=True)
+    else:
+        subprocess.call("ansible-playbook -i hosts.txt -become -u " + SOEuser + " --private-key=" + privKeyLoc + " -K main.yml", shell=True)
 
 def main():
+    osArch = discoverOsArch()
     basePath, OVATemplate, privKeyLoc, SOEuser = config()
     customerName = cusFolderSetup(basePath)
-    cloneBaseVM(customerName, basePath, OVATemplate, privKeyLoc, SOEuser)
-    startVM(customerName, basePath, OVATemplate, privKeyLoc, SOEuser)
-    vm_ip = vmIPlookup(customerName, basePath, OVATemplate, privKeyLoc, SOEuser)
-    ansibleFun(vm_ip, basePath, OVATemplate, privKeyLoc, SOEuser)
+    cloneBaseVM(customerName, basePath, OVATemplate, privKeyLoc, SOEuser, osArch)
+    startVM(customerName, basePath, OVATemplate, privKeyLoc, SOEuser, osArch)
+    vm_ip = vmIPlookup(customerName, basePath, OVATemplate, privKeyLoc, SOEuser, osArch)
+    ansibleFun(vm_ip, basePath, OVATemplate, privKeyLoc, SOEuser, osArch)
     endTime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print("[+] Completed.")
     print("[+] The start time was: " + startTime)
